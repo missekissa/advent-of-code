9810346cdffc3a9d24195b31d46860b343c6eeac
JP <83371069+missekissa@users.noreply.github.com>
2021-12-01 19:41:53 +0200

    Initial commit

---
+++ day-01/day1.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+let input = fs.readFileSync('./input.txt', {encoding: 'utf8'}).split('\n').map(n => Number(n))
+
+//Remove the whitespace from the end
+input.pop();
+
+const current = input.slice(1);
+const previous = input.slice(0, -1);
+let count = 0;
+
+current.map((element, i) => element > previous[i] ? count++ : count)
+
+console.log(count);
