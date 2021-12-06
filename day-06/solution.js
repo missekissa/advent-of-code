4e27f26efd66e279446a573d2130354c72c6f058
JP <83371069+missekissa@users.noreply.github.com>
2021-12-06 17:33:05 +0200

    Clean up day 6 solution

--- day-06/solution.js
+++ day-06/solution.js
@@ -1,34 +1,25 @@
 const fs = require("fs");
 
-//let input = fs.readFileSync("./example.txt", { encoding: "utf8" }).split(",").map(n => Number(n));
-let input = fs.readFileSync("./input.txt", { encoding: "utf8" }).split(",").map(n => Number(n));
+const input = fs.readFileSync("./input.txt", { encoding: "utf8" }).split(",").map(n => Number(n));
 
-const newInternalTimer = 8;
-const existingInternalTimer = 6;
+const timerLength = 9; //0 is valid timer value
+const timerReset = 6; //lanternfish that creates a new fish resets its timer to 6
 
 const fishCount = (day, input) => {
-    let fishes = [...Array(9).map((_, i) => i)].fill(0);
+    let fishes = [...Array(timerLength).map((_, i) => i)].fill(0);
     let array = fishes.slice();
-    // console.log(input);
-    // console.log(fishes);
-    // console.log(array);
 
     for (let fish in input) {
         fishes[input[fish]]++;
     }
 
     for (let i = 0; i < day; i++) {
-        array = fishes.slice(1);
-        array.push(fishes[0]);
-        array[6] += fishes[0];
-
+        array = fishes.slice(1).concat(fishes[0]).map((n, i) => i == timerReset ? n + fishes[0] : n);    
         fishes = array.slice();
-
     }
 
-    return fishes.reduce((previousValue, currentValue ) => previousValue + currentValue, 0);
+    return fishes.reduce((previousValue, currentValue ) => previousValue + currentValue);
 }
 
 console.log(fishCount(80, input)); //365131
 console.log(fishCount(256, input)); //1650309278600
-
