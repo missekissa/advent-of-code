7848001720b936ea1b873579a1727fe5a54e17ec
JP <83371069+missekissa@users.noreply.github.com>
2021-12-07 21:41:18 +0200

    Rewrite day 1 part 2 solution

--- day-01/solution.js
+++ day-01/solution.js
@@ -2,18 +2,26 @@ const fs = require("fs");
 
 let input = fs.readFileSync("./input.txt", { encoding: "utf8" }).split('\n').map(n => Number(n));
 
-//Remove the whitespace from the end
-input.pop();
-
-const part1 = () => {
+const part1 = (input) => {
     const current = input.slice(1);
     const previous = input.slice(0, -1);
     let count = 0;
 
-    current.map((element, i) => element > previous[i] ? count++ : count);
+    current.forEach((element, i) => element > previous[i] ? count++ : count);
 
     return count;
-
 }
 
-console.log(part1());
+const part2 = (input) => {
+    let count = 0, current, previous;
+    
+    for (let i = 1; i < input.length - 2; i++) {
+        current = input.slice(i, i + 3).reduce((previousValue, currentValue) => previousValue + currentValue);
+        previous = input.slice(i - 1, i - 1 + 3).reduce((previousValue, currentValue) => previousValue + currentValue);
+        current > previous ? count++ : 0;
+    }
+    return count;
+}
+
+console.log(part1(input));
+console.log(part2(input));
