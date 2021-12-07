6fc8ae06ac1338d3c4f60630156869af2774d5c9
JP <83371069+missekissa@users.noreply.github.com>
2021-12-07 20:16:34 +0200

    Day 7 part 2 complete

--- day-07/solution.js
+++ day-07/solution.js
@@ -1,21 +1,30 @@
 const fs = require("fs");
 const path = require('path');
-const { median } = require('mathjs');
+const { median, mean } = require('mathjs');
 
-//const input = fs.readFileSync(path.join(__dirname, "/example.txt"), { encoding: "utf8" }).split(",").map(n => Number(n));
 const input = fs.readFileSync(path.join(__dirname, "/input.txt"), { encoding: "utf8" }).split(",").map(n => Number(n));
 
-const cheapestPosition = median(input);
+const _median = median(input);
+const meanCeil = Math.ceil(mean(input));
+const meanFloor = Math.floor(mean(input));
 
-const fuelCost = (position) => {
+const fuelCost = (position, cheapestPosition) => {
     let min = Math.min(position, cheapestPosition);
     let max = Math.max(position, cheapestPosition);
     return max - min;
 }
 
-const part1 = (input) => {
-    return input.reduce((sum, crab) => sum + fuelCost(crab), 0);
+const part1 = input => input.reduce((sum, crab) => sum + fuelCost(crab, _median), 0);
+
+const triangularNumber = x => x * (x + 1) / 2;
+
+const part2 = input => {
+    let floorResult = input.reduce((sum, crab) => sum + triangularNumber(fuelCost(crab, meanFloor)), 0);
+    let ceilResult = input.reduce((sum, crab) => sum + triangularNumber(fuelCost(crab, meanCeil)), 0);
+    return [floorResult, ceilResult];
 }
 
-console.log(cheapestPosition);
+console.log("Part 1 answer is:")
 console.log(part1(input));
+console.log("Part 2 answer is either:")
+console.log(part2(input));
