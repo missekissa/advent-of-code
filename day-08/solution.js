e8176c1c44be0c8cc484f0286e1555923c0cf531
JP <83371069+missekissa@users.noreply.github.com>
2021-12-08 17:26:14 +0200

    Day 8 part 1 complete

--- day-08/solution.js
+++ day-08/solution.js
@@ -1,6 +1,15 @@
 const fs = require("fs");
 const path = require('path');
 
-const input = fs.readFileSync(path.join(__dirname, "/example.txt"), { encoding: "utf8" }).split("\n");
+const input = fs.readFileSync(path.join(__dirname, "/input.txt"), { encoding: "utf8" }).split("\n");
 
-console.log(input);
+const part1 = (input) => {
+    let output = input.map(line => line.split("| ")[1].split(" ")).flat();
+    let count = 0;
+
+    output.forEach(x => x.length == 2 || x.length == 4 || x.length == 3 || x.length == 7 ? count++ : 0);
+
+    return count;
+}
+
+console.log(part1(input));
