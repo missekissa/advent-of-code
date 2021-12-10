48e334b513ad3592522b0a03f1ab333209351ff3
JP <83371069+missekissa@users.noreply.github.com>
2021-12-10 23:39:20 +0200

    Day 10 part 2 complete

--- day-10/solution.js
+++ day-10/solution.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-const testcase = fs.readFileSync(path.join(__dirname, "/example.txt"), { encoding: "utf8" }).split("\n");
 const input = fs.readFileSync(path.join(__dirname, "/input.txt"), { encoding: "utf8" }).split("\n");
 
 const chunks = {
@@ -11,14 +10,15 @@ const chunks = {
     "<": ">"
 }
 
-const points = {
-    ")": 3,
-    "]": 57,
-    "}": 1197,
-    ">": 25137
-}
-
 const part1 = input => {
+
+    const points = {
+        ")": 3,
+        "]": 57,
+        "}": 1197,
+        ">": 25137
+    }
+
     let score = 0;
 
     input.forEach(line => {
@@ -28,11 +28,46 @@ const part1 = input => {
                 stack.push(chunks[character]);
             else if (stack.pop() != character)
                 score += points[character];
-
         }
     });
 
     return score;
 }
 
+const part2 = input => {
+    let scores = [];
+
+    const points = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4
+    }
+
+    input.forEach(line => {
+        let corrupted = false;
+        let stack = [];
+        let score = 0;
+        for (character of line) {
+            if (chunks.hasOwnProperty(character))
+                stack.push(chunks[character]);
+            else if (stack.pop() != character)
+                corrupted = true;
+        }
+        if (corrupted == false) {
+            for (character of stack.reverse()) {
+                score = score * 5 + points[character];
+            }
+            scores.push(score);
+        }
+    });
+
+    scores = scores.sort(function (a, b) {
+        return a - b;
+    });
+
+    return scores[Math.floor((scores.length - 1) / 2)];
+}
+
 console.log(part1(input));
+console.log(part2(input));
