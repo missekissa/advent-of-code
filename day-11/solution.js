eb00df0cd965d5b45a3ddf41a111f1aac67032f3
JP <83371069+missekissa@users.noreply.github.com>
2021-12-11 18:35:40 +0200

    Code cleanup

--- day-11/solution.js
+++ day-11/solution.js
@@ -3,13 +3,11 @@ const path = require("path");
 
 const parseEnergyLevels = input => input.map(line => line.match(/\d/g).map(n => Number(n)));
 
-const testcase = fs.readFileSync(path.join(__dirname, "/example.txt"), { encoding: "utf8" }).split("\n");
 const input = fs.readFileSync(path.join(__dirname, "/input.txt"), { encoding: "utf8" }).split("\n");
 
 const adjacent = (x, y, levels) => {
     let array = levels;
 
-    //Loop adjacent
     for (dx = -1; dx <= 1; ++dx) {
         for (dy = -1; dy <= 1; ++dy) {
             if (array?.[x + dx]?.[y + dy] !== undefined && array?.[x + dx]?.[y + dy] !== 0) {
@@ -22,17 +20,14 @@ const adjacent = (x, y, levels) => {
 
 const increaseEnergy = levels => levels.map(row => row.map(column => column + 1));
 
-const foo = (levels, count) => {
-    let flash = false;
-    let num = count;
-    let x, y;
-
+const flashes = (levels, count) => { 
+    let flash = false, x, y;
+   
     levels = levels.map((row, i) => {
         return row.map((column, j) => {
             if (column > 9 && flash == false) {
                 flash = true;
-                num+=1;
-                //console.log("Called!")
+                count++;
                 x = i;
                 y = j;
                 return 0;
@@ -46,21 +41,21 @@ const foo = (levels, count) => {
         levels = adjacent(x, y, levels);
     }
 
-    return flash == true ? foo(levels, num): [levels, num]; //Recursive call
+    return flash == true ? flashes(levels, count) : [levels, count]; //recursion
 }
 
-const part1 = (input) => {
+const part1 = input => {
     let count = 0;
     let energyLevels = input;
     for (let i = 0; i < 100; i++) {
         energyLevels = increaseEnergy(energyLevels);
-        [energyLevels, count] = foo(energyLevels, count);
+        [energyLevels, count] = flashes(energyLevels, count);
     }
 
     return count;
 }
 
-const part2 = (input) => {
+const part2 = input => {
     let count = 0;
     let energyLevels = input;
     let steps = 0;
@@ -68,56 +63,25 @@ const part2 = (input) => {
 
     do {
         energyLevels = increaseEnergy(energyLevels);
-        [energyLevels, count] = foo(energyLevels, count);
+        [energyLevels, count] = flashes(energyLevels, count);
 
         steps++;
         let allzeroes = true;
-        energyLevels.forEach(row => {
+        energyLevels.forEach(row =>
             row.forEach(column => {
-                if (column !== 0) {
-                    //console.log("Called!");
+                if (column !== 0)
                     allzeroes = false;
-                }
             })
-            
-        });
-        
-        if (allzeroes == true) {
+        );
+
+        if (allzeroes == true)
             simultaneously = true;
-        }
-
-
     } while (simultaneously == false)
 
     return steps;
 }
 
+const initialLevels = parseEnergyLevels(input);
 
-let energyLevels = parseEnergyLevels(testcase);
-
-let initialLevels = parseEnergyLevels(input);
-
-//console.log(part1(parseEnergyLevels(testcase)));
-
-//console.log(part1(initialLevels));
+console.log(part1(initialLevels));
 console.log(part2(initialLevels));
-
-/*
-console.log("---DEBUG---");
-//console.log(adjacent(0, 0, energyLevels));
-//console.log(energyLevels);
-//console.log("After:");
-//console.log(increaseEnergy(energyLevels));
-console.log("Before any steps:");
-console.log(energyLevels);
-
-console.log("After step 1:");
-energyLevels = increaseEnergy(energyLevels);
-energyLevels = foo(energyLevels);
-console.log(energyLevels);
-
-console.log("After step 2:");
-energyLevels = increaseEnergy(energyLevels);
-energyLevels = foo(energyLevels);
-console.log(foo(energyLevels));
-*/
